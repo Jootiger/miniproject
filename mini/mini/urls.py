6c2daedb00e9a7f100e33b96d335c8dff0424b07
User-PC\User <qkrquddyd94@gmail.com>
2018-11-06 00:32:39 +0900

    mini (main app) 폴더 urls.py에 patient 앱 추가

--- mini/mini/urls.py
+++ mini/mini/urls.py
@@ -18,6 +18,7 @@ from django.urls import path, include
 
 
 urlpatterns = [
+    path('patient/', include('patient.urls')),
     path('admin/', admin.site.urls),
     path('', include('login.urls')),
 ]
