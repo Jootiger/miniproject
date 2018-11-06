4465e044fcefcc831b44d9739b3e2019284e750a
hwangyeojin <hwangyj0202@gmail.com>
2018-11-06 16:10:26 +0900

    login.views 추가

--- mini/login/views.py
+++ mini/login/views.py
@@ -1,4 +1,9 @@
 from django.shortcuts import render
+from django.views.generic.base import TemplateView
+from django.views.generic.edit import CreateView
+from django.contrib.auth.forms import UserCreationForm
+from django.urls import reverse_lazy
+
 
 # Create your views here.
 
@@ -6,6 +11,31 @@ def index_dlg(request):
     context = {}
     return render(request, 'pages/index.html', context)
 
-def login_dlg(request):
+def login_select(request):
     context = {}
-    return render(request, 'pages/login.html', context)
+    return render(request, 'pages/login_select.html', context)
+
+def login_hospital(request):
+    context = {}
+    return render(request, 'pages/login_hospital.html', context)
+
+def login_patient(request):
+    context = {}
+    return render(request, 'pages/login_patient.html', context)
+
+def register_select(request):
+    context = {}
+    return render(request, 'pages/register_select.html', context)
+
+class PatientCreateView(CreateView):
+    template_name = "pages/register_patient.html"
+    form_class = UserCreationForm
+    success_url = reverse_lazy("register_done")
+
+class HospitalCreateView(CreateView):
+    template_name = "pages/register_hospital.html"
+    form_class = UserCreationForm
+    success_url = reverse_lazy("register_done")
+
+class UserCreateDone(TemplateView):
+    template_name = "pages/register_done.html"
