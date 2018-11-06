f5c833372c9e645c36d80bad94d943a0d45f8343
hwangyeojin <hwangyj0202@gmail.com>
2018-11-06 16:06:58 +0900

    login.urls

--- mini/login/urls.py
+++ mini/login/urls.py
@@ -1,7 +1,16 @@
-from django.conf.urls import url
-from . import views
+from django.urls import path, include 
+from django.views.generic.base import TemplateView
+from login import views
+from login.views import *
+
+app_name = 'login'
 
 urlpatterns = [
-    url(r'^$', views.index_dlg, name='index_dlg'),
-    url(r'^login', views.login_dlg, name='login_dlg'),    
+    path('', views.index_dlg, name='index_dlg'),
+    path('login', views.login_select, name='login_select'),
+    path('login/hospital/', views.login_hospital, name='login_hospital'),
+    path('login/patient/', views.login_patient, name='login_patient'),
+    path('register', views.register_select, name='register_select'),
+    path('register/hospital/', PatientCreateView.as_view(), name="register_hospital"),
+    path('register/patient/', HospitalCreateView.as_view(), name="register_patient"),
 ]
