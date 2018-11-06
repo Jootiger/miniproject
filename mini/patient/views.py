2d1cefe4cab0995e2adf588f5cb9da2cf3ffc24c
ByungYongPark <qkrquddyd94@naver.com>
2018-11-06 15:44:51 +0900

    patient 앱의 views에서 환자별 목록 리스트 볼 수 있도록 코드 수정

--- mini/patient/views.py
+++ mini/patient/views.py
@@ -1,19 +1,25 @@
-from django.shortcuts import render,get_object_or_404
+from django.shortcuts import render,get_object_or_404,get_list_or_404
 from django.http import HttpResponse
-from .models import Info
+from patient.models import Info
+
 # Create your views here.	# Create your views here.
-def file_list(request,patient_id):
-    files=Info.objects.get(pk=patient_id).order_by('id')
+def file_list_all(request):
+    files=Info.objects.order_by('pid')
     context={}
     context['files']=files
 
     return render(request,'patient/file_list.html',context)
 
-def file_download(request, patient_id):
-    context={}
-    if patient_id:
-        files = get_object_or_404(Info,pk=patient_id)
-        context['files']=files
-    else:
-        return render(request, 'patient/file_list.html',context)
+def file_download(request):
     return HttpResponse('파일 다운로드')
+    
+def file_list(request,patient_pid):
+    # files=get_object_or_404(Info)
+    # files=Info.objects.get(pk=patient_pid)
+    # files=Info.objects.order_by('id')
+    # files=Info.objects.all()
+    files=get_list_or_404(Info,pid=patient_pid)
+    context={}
+    context['files']=files
+
+    return render(request,'patient/file_list.html',context)
