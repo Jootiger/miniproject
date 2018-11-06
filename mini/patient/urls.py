351642a7bbfae3ac91c4bbc7e7334b0cbe5e3fdd
ByungYongPark <qkrquddyd94@naver.com>
2018-11-06 15:44:27 +0900

    patient 앱의 urls에 파라미터를 환자 pid로 수정

--- mini/patient/urls.py
+++ mini/patient/urls.py
@@ -2,6 +2,9 @@ from django.urls import path
 from . import views
 app_name='patient'
 urlpatterns = [
-    path('<int:patient_id>/file/', views.file_list, name='file_list'),
-    path('<int:patient_id>/file/download/', views.file_download, name='file_download'),
+    path('', views.file_list_all, name='file_list_all'),
+    path('file/', views.file_list_all, name='file_list'),
+    path('file/download/', views.file_download, name='file_download'),
+    path('<int:patient_pid>/file/', views.file_list, name='file_list'),
+    path('<int:patient_pid>/file/download/', views.file_download, name='file_download'),
 ]
