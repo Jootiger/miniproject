5ac13b39787d190322724cbb8a57981c6daf152e
ByungYongPark <qkrquddyd94@naver.com>
2018-11-06 13:42:14 +0900

    patient 모델 추가

--- mini/patient/models.py
+++ mini/patient/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+class Info(models.Model):
+    name = models.CharField('NAME', max_length=50)
+    pid = models.IntegerField('Patient ID', unique=True, blank=True, null=True)
+    description = models.CharField('DESCRIPTION', max_length=100, blank=True, null=True)
+    content = models.TextField('CONTENT')
+    createDate = models.DateTimeField('Create Date', auto_now_add=True)
+    modifyDate = models.DateTimeField('Modify Date', auto_now=True)
+    owner = models.ForeignKey(User, null=True, on_delete=models.CASCADE, related_name='%(class)s_patient_models')
